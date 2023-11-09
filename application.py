66f62d9accc7bc9ef5a9653b60632b457cb19185
truongnn <74360292+truong11062002@users.noreply.github.com>
2023-11-09 11:08:03 +0700

    update application for xgr reg

--- application.py
+++ application.py
@@ -6,7 +6,7 @@ import numpy as np
 
 app=Flask(__name__)
 cors=CORS(app)
-main_model = pickle.load(open('./models/XGBReg.pkl','rb'))
+main_model = pickle.load(open('./models/xgb_r.pkl','rb'))
 car=pd.read_csv('./data/cleaned_car_v3.csv')
 
 @app.route('/',methods=['GET','POST'])
@@ -34,7 +34,7 @@ def index():
 @cross_origin()
 def predict():
 
-    brand = request.form.get('company')
+    brand = request.form.get('companies')
     model = request.form.get('car_models')
     
     manufacture_date = request.form.get('year')
@@ -42,14 +42,12 @@ def predict():
     mileage_v2 = request.form.get('kilo_driven')
     condition = request.form.get('condition')
     type_car = request.form.get('type_car')
-    curr_year = pd.Timestamp.now().year
-    years_since_manufacture = curr_year - int(manufacture_date)
+    # curr_year = pd.Timestamp.now().year
+    # years_since_manufacture = curr_year - int(manufacture_date)
     
     
-    prediction= main_model.predict(pd.DataFrame(columns=['manufacture_date', 'brand', 'model', 'type_car', 'fuel', 'condition', 'mileage_v2', 'years_since_manufacture'],
-                              data=np.array([manufacture_date, brand, model, type_car, fuel, condition, mileage_v2, years_since_manufacture]).reshape(1,8)))
-    print(prediction)
-
+    prediction = main_model.predict(pd.DataFrame(columns=['brand', 'model', 'manufacture_date', 'fuel', 'mileage_v2', 'condition', 'type_car'],
+       data=np.array([brand, model, manufacture_date, fuel, mileage_v2, condition, type_car]).reshape(1,7)))
     # Return VND
     return str('{:,.0f}'.format(prediction[0]))
 
